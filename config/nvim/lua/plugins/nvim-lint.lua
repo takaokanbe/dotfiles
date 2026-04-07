e4e9db30916651bf9122177deff17e2bad6d8385
takaokanbe <62293842+takaokanbe@users.noreply.github.com>
2026-04-07 18:59:52 +0900

    fix: check linter executable before running nvim-lint

--- config/nvim/lua/plugins/nvim-lint.lua
+++ config/nvim/lua/plugins/nvim-lint.lua
@@ -12,7 +12,14 @@ return {
     vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
       group = vim.api.nvim_create_augroup("Linting", { clear = true }),
       callback = function()
-        lint.try_lint()
+        local ft = vim.bo.filetype
+        local linters = lint.linters_by_ft[ft] or {}
+        local available = vim.tbl_filter(function(name)
+          return vim.fn.executable(lint.linters[name] and lint.linters[name].cmd or name) == 1
+        end, linters)
+        if #available > 0 then
+          lint.try_lint(available)
+        end
       end,
     })
   end,
